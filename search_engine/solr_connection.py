66ea3cc3f03bc93c4da286a93f0c1e6f060f06ba
tritranhuu <tritranhuu123@gmail.com>
2019-10-23 09:01:02 +0700

    moduling, webapp

--- search_engine/solr_connection.py
+++ search_engine/solr_connection.py
@@ -1,6 +1,5 @@
 import pysolr
 
-
 class SolrConnection:
     def __init__(self, path):
         self.path = path
@@ -10,7 +9,7 @@ class SolrConnection:
         self.connection.add(docs, commit=True)
 
     def search(self, term):
-        results = self.connection.search(q=f'title:"{term}"')
+        results = self.connection.search(q="*:*")
         return results
 
     def delete(self, doc_id, q):
@@ -18,3 +17,9 @@ class SolrConnection:
 
     def delete_all(self):
         self.connection.delete(q="*:*", commit=True)
+
+
+if __name__ == "__main__":
+        s = SolrConnection("http://localhost:8983/solr/tktdtt/")
+        a = s.search("")
+        print(a.raw_response['response']['docs'])
